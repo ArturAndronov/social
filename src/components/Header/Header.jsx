7869379dc2b26c190fbce1bfdf87260a66fa5a75
AndronovArtur <artik_andronov@mail.ru>
2022-11-07 12:29:28 +0200

    module.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import s from './Header.module.css';
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src='https://i0.wp.com/esparadis.co.uk/wp-content/uploads/2018/04/instagram-Logo-PNG-Transparent-Background-download.png?fit=1000%2C1000&ssl=1' />
+        </header>
+    )
+}
+export default Header;
