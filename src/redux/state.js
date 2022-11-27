ad19ebe2e01af2d5af01f35b8b6d28165fe3888d
AndronovArtur <artik_andronov@mail.ru>
2022-11-27 12:13:48 +0200

    FLUX; обнуление поста, рефакторинг кода

--- src/redux/state.js
+++ src/redux/state.js
@@ -8,7 +8,8 @@ let state = {
             { id: 2, message: 'It\'s my first post', likesCounts: '20' },
             { id: 3, message: 'sagasgasgasgasgas', likesCounts: '24' },
             { id: 4, message: 'bbbbbbbbbbbbbbbbbbb', likesCounts: '266' }
-        ]
+        ],
+        newPostText: 'Новый пост'
     },
     dialogsPage: {
         messages: [
@@ -30,14 +31,20 @@ let state = {
     }
 
 }
-
-export let addPost = (postMessage) => {
+window.state = state;
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCounts: 0
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
--- src/render.js
+++ src/render.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost } from './redux/state';
+import { addPost, updateNewPostText } from './redux/state';
   
-export let rerenderEntireTree = (state) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
+export let rerenderEntireTree = (state) => {
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App state={state} addPost={addPost}/>
+      <App state={state} 
+           addPost={addPost} 
+           updateNewPostText={updateNewPostText}/>
     </React.StrictMode>
   </BrowserRouter>
 );
 }
-
