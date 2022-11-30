8dff2f110b91a9622173c18e60bb17fd6252b5d3
AndronovArtur <artik_andronov@mail.ru>
2022-11-30 12:01:43 +0200

    store; callback; refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updateNewPostText } from './redux/state';
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
@@ -13,16 +12,16 @@ root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App state={state} 
-           addPost={addPost} 
-           updateNewPostText={updateNewPostText}/>
+           addPost={store.addPost.bind(store)} 
+           updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>
   </BrowserRouter>
 );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,57 +1,61 @@
-let rerenderEntireTree = () => {
-    console.log('state change');
-}
-let state = {
+let store = {
+    _state: {
 
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCounts: '15' },
-            { id: 2, message: 'It\'s my first post', likesCounts: '20' },
-            { id: 3, message: 'sagasgasgasgasgas', likesCounts: '24' },
-            { id: 4, message: 'bbbbbbbbbbbbbbbbbbb', likesCounts: '266' }
-        ],
-        newPostText: 'Новый пост'
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you?', likesCounts: '15' },
+                { id: 2, message: 'It\'s my first post', likesCounts: '20' },
+                { id: 3, message: 'sagasgasgasgasgas', likesCounts: '24' },
+                { id: 4, message: 'bbbbbbbbbbbbbbbbbbb', likesCounts: '266' }
+            ],
+            newPostText: 'Новый пост'
+        },
+        dialogsPage: {
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How are you' },
+                { id: 3, message: 'Wassup ' },
+                { id: 4, message: 'good' },
+                { id: 5, message: 'job' },
+                { id: 6, message: 'asff' }
+            ],
+            dialogs: [
+                { id: 1, name: 'Arthur', img: 'https://cdn-icons-png.flaticon.com/512/74/74291.png' },
+                { id: 2, name: 'Ivan', img: 'https://cdn-icons-png.flaticon.com/512/53/53154.png' },
+                { id: 3, name: 'Evgeniy', img: 'https://cdn-icons-png.flaticon.com/512/53/53081.png' },
+                { id: 4, name: 'Alexandr', img: 'https://cdn-icons-png.flaticon.com/512/56/56990.png' },
+                { id: 5, name: 'Andrew', img: 'https://cdn-icons-png.flaticon.com/512/53/53092.png' },
+                { id: 6, name: 'Vladimir', img: 'https://cdn-icons-png.flaticon.com/512/53/53092.png' }
+            ]
+        }
+
     },
-    dialogsPage: {
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How are you' },
-            { id: 3, message: 'Wassup ' },
-            { id: 4, message: 'good' },
-            { id: 5, message: 'job' },
-            { id: 6, message: 'asff' }
-        ],
-        dialogs: [
-            { id: 1, name: 'Arthur', img: 'https://cdn-icons-png.flaticon.com/512/74/74291.png' },
-            { id: 2, name: 'Ivan', img: 'https://cdn-icons-png.flaticon.com/512/53/53154.png' },
-            { id: 3, name: 'Evgeniy', img: 'https://cdn-icons-png.flaticon.com/512/53/53081.png' },
-            { id: 4, name: 'Alexandr', img: 'https://cdn-icons-png.flaticon.com/512/56/56990.png' },
-            { id: 5, name: 'Andrew', img: 'https://cdn-icons-png.flaticon.com/512/53/53092.png' },
-            { id: 6, name: 'Vladimir', img: 'https://cdn-icons-png.flaticon.com/512/53/53092.png' }
-        ]
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('state change');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCounts: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;   //patern
     }
 
 }
-window.state = state;
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCounts: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;   //patern
-}
-
-export default state;
+export default store;
+window.store = store;
