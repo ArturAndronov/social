4f822d4c8376f80107edadcf6215bbfae68c2315
AndronovArtur <artik_andronov@mail.ru>
2023-02-20 11:40:02 +0200

    Updates

---
+++ precache-manifest.3a2edf541e4d904e2dbc898d102d8d74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81d6bb8f8560b4bb8444dcf663321aa",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "821259bb603e65c50aab",
+    "url": "/social/static/css/3.670e50e8.chunk.css"
+  },
+  {
+    "revision": "454ba733758c4d3fa9d9",
+    "url": "/social/static/css/main.3987b77b.chunk.css"
+  },
+  {
+    "revision": "990068c07c17eadaa001",
+    "url": "/social/static/js/2.ffbf4ee5.chunk.js"
+  },
+  {
+    "revision": "821259bb603e65c50aab",
+    "url": "/social/static/js/3.c06a0337.chunk.js"
+  },
+  {
+    "revision": "fd2775833c516d0e2287",
+    "url": "/social/static/js/4.135f8f2b.chunk.js"
+  },
+  {
+    "revision": "454ba733758c4d3fa9d9",
+    "url": "/social/static/js/main.e468ad47.chunk.js"
+  },
+  {
+    "revision": "9bfbc758fe165e42b03e",
+    "url": "/social/static/js/runtime~main.6410f650.js"
+  },
+  {
+    "revision": "3b76da2629e95159ccc07c6617a9e6d3",
+    "url": "/social/static/media/loader-loading.3b76da26.gif"
+  },
+  {
+    "revision": "9963a3ea5c9be0060450c50f0f2bbe46",
+    "url": "/social/static/media/user.9963a3ea.png"
+  }
+]);
