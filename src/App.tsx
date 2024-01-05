f24d10b1c9a51a5f3d76c551db2d0b289b4f6035
Artur <artik_andronov@mail.ru>
2024-01-05 17:15:25 +0200

    update layout APP AntDesign

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
   useNavigate,
   useParams,
   Navigate,
+  NavLink,
+  Link,
 } from "react-router-dom";
 import './App.css';
 
@@ -24,6 +26,12 @@ import Preloader from './components/common/Preloader/Preloader.tsx';
 import { AppStateType } from './redux/redux-store.ts';
 import { withSuspens } from './hoc/withSuspense.tsx';
 import { UsersPage } from './components/Users/UsersContainer.tsx';
+
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import { Header } from './components/Header/Header.tsx';
+const { Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu
 
 //import DialogsContainer from './components/Dialogs/DialogsContainer';
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer.tsx'));
@@ -43,32 +51,87 @@ class App extends Component<MapPropsType & DispatchPropsType> {
   }
 
   render() {
-    if(!this.props.initialized){
+    if (!this.props.initialized) {
       return <Preloader />
     }
-
-
     return (
+      <Layout>
+        <Header/>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                /*  defaultSelectedKeys={['7']}*/
+                /*  defaultOpenKeys={['sub1']}*/
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="My Profile">
+                  <Menu.Item key="1"> <Link to="/profile">Profile</Link></Menu.Item>
+                  <Menu.Item key="2"> <Link to="/dialogs">Messages</Link></Menu.Item>
+                  <Menu.Item key="3">option3</Menu.Item>
+                  <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="Users">
+                  <Menu.Item key="5"><Link to="/Users">Users</Link></Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                  <Menu.Item key="9">option9</Menu.Item>
+                  <Menu.Item key="10">option10</Menu.Item>
+                  <Menu.Item key="11">option11</Menu.Item>
+                  <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+              <Suspense fallback={<div>LOADING....</div>}>
+                <Routes>
+                  <Route path='/profile/:userId' element={<ProfileContainer />} />
+                  <Route path="/" element={<Navigate to="/profile" />} />
+                  <Route path='/profile' element={<SuspendedProfile />} />
+                  <Route path='/users' element={<UsersPage pageTitle='Social' />} />
+                  <Route path='/login' element={<Login />} />
+                  <Route path='/dialogs' element={<SuspendedDialogs />} />
+                  <Route path='/news' element={<News />} />
+                  <Route path='/music' element={<Music />} />
+                  <Route path='/settings' element={<Settings />} />
+                </Routes>
+              </Suspense>
 
-      <div className='app-wrapper'>
-        <HeaderContainer />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Suspense fallback={<div>LOADING....</div>}>
-            <Routes>
-              <Route path='/profile/:userId' element={<ProfileContainer />} />
-              <Route path="/" element={<Navigate to="/profile" />} />
-              <Route path='/profile' element={<SuspendedProfile />} />
-              <Route path='/users' element={<UsersPage pageTitle='Social'/>} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/dialogs' element={<SuspendedDialogs />} />
-              <Route path='/news' element={<News />} />
-              <Route path='/music' element={<Music />} />
-              <Route path='/settings' element={<Settings />} />
-            </Routes>
-          </Suspense>
-        </div>
-      </div>
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Social Network ©2024 Created by Artur A.S.</Footer>
+      </Layout>
+
+
+      // <div className='app-wrapper'>
+      //   <HeaderContainer />
+      //   <Navbar />
+      //   <div className='app-wrapper-content'>
+      //     <Suspense fallback={<div>LOADING....</div>}>
+      //       <Routes>
+      //         <Route path='/profile/:userId' element={<ProfileContainer />} />
+      //         <Route path="/" element={<Navigate to="/profile" />} />
+      //         <Route path='/profile' element={<SuspendedProfile />} />
+      //         <Route path='/users' element={<UsersPage pageTitle='Social'/>} />
+      //         <Route path='/login' element={<Login />} />
+      //         <Route path='/dialogs' element={<SuspendedDialogs />} />
+      //         <Route path='/news' element={<News />} />
+      //         <Route path='/music' element={<Music />} />
+      //         <Route path='/settings' element={<Settings />} />
+      //       </Routes>
+      //     </Suspense>
+      //   </div>
+      // </div>
 
     )
   };
@@ -97,3 +160,5 @@ function withRouter(Component) {
 export default compose(
   withRouter,
   connect(mapStateToProps, { initializeApp }))(App);
+
+
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,62 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import s from './Header.module.css';
 
+import { Avatar, Button, Col, Layout, Menu, Row } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentUserLogin, selectIsAuth } from '../../redux/auth-selectors.ts';
+import { logout } from '../../redux/auth-reducer.ts';
+
 export type MapPropsType = {
-    isAuth: boolean
-    login: string | null
-}
-export type DispatchPropsType = {
-    logout: () => void
 }
 
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+
+export const Header: React.FC<MapPropsType> = (props) => {
+    const isAuth = useSelector(selectIsAuth)
+    const login = useSelector(selectCurrentUserLogin)
+
+    const dispatch = useDispatch()
+
+    const logoutCallback = () => {
+        dispatch(logout())
+    }
+
+    const { Header } = Layout;
     return (
-        <header className={s.header}>
-            <img src='https://cdn-icons-png.flaticon.com/512/5969/5969833.png' alt='logo'/>
+        <Header className='header'>
+            <div className='logo' />
+            <Row>
+                <Col span={18}>
+                    <Menu theme='dark' mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu.Item key="1"><Link to="/users">Users</Link></Menu.Item>
+                    </Menu>
+                </Col>
+                {isAuth
+                    ? <> <Col span={1}>
+                        <Avatar alt={login || ''} style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                    </Col>
+                        <Col span={5}>
+                            <Button onClick={logoutCallback}>Log out</Button>
+                        </Col>
+                    </>
+                    : <Col span={6}>
+                        <Button>
+                            <Link to={'/login'}>Login</Link>
+                        </Button>
+                    </Col>}
+            </Row>
+        </Header >
 
-            <div className={s.loginBlock}>
-                {props.isAuth
-                    ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
-                    : <NavLink to={'/login'}>Login</NavLink>}
-            </div>
-        </header>
+
+        // <header className={s.header}>
+        //     <img src='https://cdn-icons-png.flaticon.com/512/5969/5969833.png' alt='logo'/>
+
+        //     <div className={s.loginBlock}>
+        //         {props.isAuth
+        //             ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
+        //             : <NavLink to={'/login'}>Login</NavLink>}
+        //     </div>
+        // </header>
     )
 }
-export default Header;
